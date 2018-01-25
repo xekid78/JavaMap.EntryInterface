f639f3e7f04163a993654fa5222556239f1792f7
Makoto Tanabe <xekid78@outlook.com>
2018-01-25 21:52:27 +0900

    Map.Entryインタフェース

---
+++ Sample38.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class Sample38 {
+
+	public static void main(String[] args) {
+		HashMap<String, String> enemyMap = new HashMap<String, String>();
+		enemyMap.put("ザコ", "スライム");
+		enemyMap.put("中ボス", "ドラゴン");
+		enemyMap.put("ラスボス", "魔王");
+		for (Entry<String, String> entry : enemyMap.entrySet()) {
+			System.out.print(entry.getKey() + ",");
+			System.out.println(entry.getValue());
+		}
+
+	}
+
+}
